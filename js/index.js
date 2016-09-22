1ddb5e5a5acff4424e87f3f90e844ae650f198c9
P. Mohan <ngapigyi@gmail.com>
2016-09-22 10:16:41 +0000

    Initial design commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    $('body').on('click', '.page-scroll a', function (event) {
+        var $anchor = $($(this).attr('href')).offset().top;
+        $('html, body').stop().animate({
+            scrollTop: $anchor
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
---
+++ pages/blogs.md
@@ -0,0 +1,17 @@
+---
+layout: default
+title: Blogs
+permalink: /blog/
+redirect_from:
+  - /blogs.html
+sitemap:
+    priority: 0.7
+    lastmod: 2016-09-22T00:00:00-00:00
+---
+
+
+<h3 align="center"> Blogs coming soon! <h3>
+
+<br>
+
+<h3 align="center">Thanks for visiting. <h3>
